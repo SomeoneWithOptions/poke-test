4b71450d28e8659d677cf1f5c9a7d8fdd5a18222
SomeoneWithOptions <andrescastesol@gmail.com>
2022-11-12 14:36:30 -0500

    change img for Next Image tag

--- pages/PokeImage.js
+++ pages/PokeImage.js
@@ -1,8 +1,10 @@
+import Image from "next/image";
+
 export const PokeImage = ({ number, shiny }) => {
   let source = !shiny
     ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png`
     : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${number}.png`;
-  return <img src={source} width={200} height={200} alt ='Pokemon Image'/>;
+  return <Image src={source} width={200} height={200} alt ='Pokemon Image'/>;
 };
 
 export default PokeImage;
