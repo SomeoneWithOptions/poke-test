0a82f472f75f16ecec48d4ef2c7535cc31200888
SomeoneWithOptions <andrescastesol@gmail.com>
2022-11-13 09:31:41 -0500

    removed css media querys for clamp and Removed NextJS image Optimization

--- pages/PokeImage.js
+++ pages/PokeImage.js
@@ -4,7 +4,7 @@ export const PokeImage = ({ number, shiny }) => {
   let source = !shiny
     ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png`
     : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${number}.png`;
-  return <Image src={source} width={200} height={200} alt ='Pokemon Image'/>;
+  return <Image src={source} width={200} height={200} alt ='Pokemon Image' unoptimized/>;
 };
 
 export default PokeImage;
